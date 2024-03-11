b96fc97f20709428d9ac8a08c684615fb7a69899
mazinalla <mazinelsadig8@gmail.com>
2024-03-11 15:57:19 +0300

    clone

--- console.py
+++ console.py
@@ -1,138 +1,32 @@
 import cmd
 import sys
-import json
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
-        """Exit the program"""
-        return True
+    def do_help(self, args):
+        """Get help on commands
+           'help' or '?' with no arguments prints a list of commands for which help is available
+           'help <command>' or '? <command>' gives help on <command>
+        """
+        ## The only reason to define this method is for the help text in the doc string
+        cmd.Cmd.do_help(self, args)
 
     def do_EOF(self, arg):
-        """Exit the program"""
-        print("")  # Print a new line for better formatting
-        return True
-
-    def do_help(self, arg):
-        """Show help"""
-        super().do_help(arg)
-
-    def emptyline(self):
-        """Do nothing on empty line"""
+        """Exit on system end of file"""
+        return -1
+    
+    def do_quit(self, arg):
+        """ exit from the console """
+        return -1
+    
+    def emptyline(self):    
+        """Do nothing on empty input line"""
+        
         pass
 
-    def do_create(self, arg):
-        """Create a new instance of BaseModel"""
-        if not arg:
-            print("** class name missing **")
-            return
-
-        try:
-            new_instance = eval(arg)()
-            new_instance.save()
-            print(new_instance.id)
-        except NameError:
-            print("** class doesn't exist **")
-
-    def do_show(self, arg):
-        """Show the string representation of an instance"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in storage.classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        obj_id = args[1]
-        key = f"{class_name}.{obj_id}"
-        if key in storage.all():
-            print(storage.all()[key])
-        else:
-            print("** no instance found **")
-
-    def do_destroy(self, arg):
-        """Destroy an instance"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in storage.classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        obj_id = args[1]
-        key = f"{class_name}.{obj_id}"
-        if key in storage.all():
-            del storage.all()[key]
-            storage.save()
-        else:
-            print("** no instance found **")
-
-    def do_all(self, arg):
-        """Show all instances"""
-        args = arg.split()
-        if args and args[0] not in storage.classes:
-            print("** class doesn't exist **")
-            return
-        result = []
-        for key, obj in storage.all().items():
-            if not args or args[0] == obj.__class__.__name__:
-                result.append(str(obj))
-        print(result)
-
-    def do_update(self, arg):
-        """Update an instance attribute"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in storage.classes:
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        obj_id = args[1]
-        key = f"{class_name}.{obj_id}"
-        if key not in storage.all():
-            print("** no instance found **")
-            return
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-        attribute_name = args[2]
-        if len(args) < 4:
-            print("** value missing **")
-            return
-        value = args[3]
-        obj = storage.all()[key]
-        setattr(obj, attribute_name, eval(value))
-        obj.save()
-
-if __name__ == "__main__":
-
-    if len(sys.argv) > 1:
-        # Non-interactive mode, read commands from a file or pipe
-        with open(sys.argv[1], 'r') as script_file:
-            commands = script_file.readlines()
-            HBNBCommand().onecmd("\n".join(commands))
-    else:
-        # Interactive mode
-        HBNBCommand().cmdloop()
-    storage.reload()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
 
